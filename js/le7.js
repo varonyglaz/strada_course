528d60c50f3a79b3547e48c40b0b4f2089f67322
varonyglaz <varonyglaz@gmail.com>
2022-08-11 19:22:01 +0300

    lesson 7

---
+++ js/le7.js
@@ -0,0 +1,9 @@
+let num1 = 5;
+let num2 = 8;
+console.log(num1 < num2);
+let result = num1 > num2;
+console.log(result);
+let char1 = 'haa';
+let char2 = 'hhh';
+console.log(char1 > char2);
+console.log('005' === 5);
