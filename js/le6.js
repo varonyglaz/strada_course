630848e0b67cfe8da9c1363bbe1054c539ba321a
Nikita <varonyglaz@gmail.com>
2022-08-11 17:18:06 +0300

    First Commit

---
+++ js/le6.js
@@ -0,0 +1,25 @@
+let height = 'hello';
+height = String(height);
+height = Number(height);
+console.log(typeof height);
+console.log(height);
+let num1 = '12';
+let num2 = '6';
+console.log(num1 - num2);
+console.log(Boolean(num1));
+num1 = Number(num1);
+num2 = Number(num2);
+num1 = -num1;
+console.log(num1);
+console.log(num2);
+console.log(num1 + num2);
+console.log(15 % 4);
+console.log(144 ** (1/2));
+//num1 = String(num1);
+console.log(typeof +num1);
+console.log(+num1 + num2);
+num1 += 5;
+console.log(num1);
+num1 = (1 + 2, 3 + 9);
+console.log(num1);
+
