325411178ee52d8817f2536c3d395eb72b054522
varonyglaz <varonyglaz@gmail.com>
2022-08-12 23:36:29 +0300

    Calc + lesson 8

---
+++ js/le8.js
@@ -0,0 +1,27 @@
+// let year = prompt('What year is now?', '');
+// if (year == 2022) {
+//   alert('You BITCH');
+// } else if (year > 2022) {
+//   alert('You fucking McFly');
+// } else {
+//   alert('Fuck you');
+// }
+// let age = 19;
+// let access;
+// let result = (access = age > 18 ? 'You proshol' : false);
+// alert(result);
+
+// let age = prompt('AGE?', '');
+// let message =
+//   (age < 3)
+//     ? 'Go fuck'
+//     : (age < 18 && age >= 3)
+//     ? 'Hi nigga'
+//     : (age < 100)
+//     ? 'Hello pussy'
+//     : 'Hi dude';
+// alert(message);
+
+let num1 = 'h';
+
+console.log(Number(num1));
