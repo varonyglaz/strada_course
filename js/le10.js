e52102ef82127ea510a1a6fdb37739375ad8141f
varonyglaz <varonyglaz@gmail.com>
2022-08-12 23:47:10 +0300

    Lesson 10

--- js/le10.js
+++ js/le10.js
@@ -1,2 +1,14 @@
-let a = 10;
-console.log(typeof a);
+let a = 15;
+switch (a) {
+  case 1:
+    console.log('too small');
+    break;
+  case 10:
+    console.log('size does matter after all');
+    break;
+  case 15:
+    console.log('too big');
+    break;
+  default:
+    console.log('Hello');
+}
